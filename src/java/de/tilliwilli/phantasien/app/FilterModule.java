b910b27b874cb727f9cc1331a128fe53f1b4dc36
tilliwilli <tilman.wilhelm@googlemail.com>
2012-09-27 15:59:18 +0200

    changed FilterModule to be a simple Guice ServletModule instead of Lime's MvcModule

--- src/java/de/tilliwilli/phantasien/app/FilterModule.java
+++ src/java/de/tilliwilli/phantasien/app/FilterModule.java
@@ -2,19 +2,16 @@ package de.tilliwilli.phantasien.app;
 
 import java.util.Map;
 
-import org.zdevra.guice.mvc.MvcModule;
-
 import com.google.common.collect.Maps;
+import com.google.inject.servlet.ServletModule;
 import com.googlecode.objectify.ObjectifyFilter;
 
-public class FilterModule extends MvcModule {
+public class FilterModule extends ServletModule {
 
 	@Override
-	protected void configureControllers() {
-		// map used for filter parameters
-		Map<String, String> params = Maps.newHashMap();
-
+	protected void configureServlets() {
 		// set CharacterEncodingFilter to make all requests have a UTF-8 encoding
+		Map<String, String> params = Maps.newHashMap();
 		params.put("encoding", "UTF-8");
 		filter("/*").through(CharacterEncodingFilter.class, params);
 
